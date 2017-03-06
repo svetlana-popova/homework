6dc06d45f069a7ad4a5212f7736eb133c87d2204
svetlana-popova <3296529@gmail.com>
2017-03-06 18:51:09 +0300

    Update TriangleMatrix.cpp

--- TriangleMatrix.cpp
+++ TriangleMatrix.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 using namespace std;
 int** AllocateMemory(int, int*);
-int** AllocateQuadMemory(int);
 int* AllocateMemory(int);
-void RandomArray(int**, int, int*);
 void DisplayArray(int**, int, int*);
-void DisplayArray(int**, int);
 void ArrayConsecutiveNumbers(int* a, int n);
 void EnterTriangleArray(int**, int*, int);
 void TransposeMatrix(int**, int*, int);
@@ -19,14 +16,11 @@ int main()
 	cin >> n;
 	int* lengths = AllocateMemory(n);
 	ArrayConsecutiveNumbers(lengths, n);
-	int** matrix = AllocateQuadMemory(n);
-	DisplayArray(matrix, n);
-	cout << endl;
+	int** matrix = AllocateMemory(n, lengths);
 	EnterTriangleArray(matrix, lengths, n);
-	DisplayArray(matrix, n);
+	DisplayArray(matrix, n, lengths);
 	cout << endl;
 	TransposeMatrix(matrix, lengths, n);
-	DisplayArray(matrix, n);
 	cout << endl;
 
 }
@@ -50,17 +44,6 @@ int** AllocateMemory(int n, int* arr)
 	return matrix;
 }
 
-int** AllocateQuadMemory(int n)
-{
-	int** matrix = new int*[n];
-	for (int i = 0; i < n; i++)
-	{
-		matrix[i] = new int[n];
-		for (int j = 0; j < n; j++)
-			matrix[i][j] = 0;
-	}
-	return matrix;
-}
 
 int* AllocateMemory(int n)
 {
@@ -70,20 +53,6 @@ int* AllocateMemory(int n)
 	return a;
 }
 
-void RandomArray(int** matrix, int n, int* arr)
-{
-	for (int i = 0; i < n; i++)
-	{
-		for (int g = 0; g < arr[i]; g++)
-			matrix[i][g] = rand() % 10;
-	}
-}
-
-void RandomArray(int* arr, int n)
-{
-	for (int i = 0; i < n; i++)
-		arr[i] = rand() % 10;
-}
 
 void DisplayArray(int** matrix, int n, int* arr)
 {
@@ -98,24 +67,6 @@ void DisplayArray(int** matrix, int n, int* arr)
 	}
 }
 
-void DisplayArray(int** matrix, int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-
-		{
-			for (int g = 0; g < n; g++)
-				cout << matrix[i][g] << " ";
-		}
-		cout << endl;
-	}
-}
-
-void DisplayArray(int* arr, int n)
-{
-	for (int i = 0; i < n; i++)
-		cout << arr[i] << " ";
-}
 
 void EnterTriangleArray(int** matrix, int* lengths, int n)
 {
@@ -131,8 +82,9 @@ void TransposeMatrix(int** matrix, int* lengths, int n)
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < i; j++)
-		{
-			matrix[j][i] = matrix[n - j][n - i];
-		}
+			cout << matrix[i][j] << " ";
+		for (int k = i + 1; k < n; k++)
+			cout << matrix[k][i] << " ";
+		cout << endl;
 	}
 }
